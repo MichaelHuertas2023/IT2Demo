35a97bcac12c5e0585fe5298566e2d151600436e
MichaelHuertas2023 <145421965+MichaelHuertas2023@users.noreply.github.com>
2023-12-10 21:16:59 +0800

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,13 @@
+function details(){
+    alert('You are Michael Oliver Huertas!');
+}
+
+function goback(){
+    location.href = www.google.com;
+}
+
+document.getElementById("menu").onclick = function() {
+    location.href = "www.google.com";
+};
+
+console.log('Hello ISPSC');
